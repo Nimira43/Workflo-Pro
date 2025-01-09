fd27f83f0e8be7732f148a998e4660e3ce37fed9
Nimira43 <n.rabone72@aol.com>
2025-01-09 02:18:39 +0000

    Setting up user data inside users.ts

---
+++ src/app/users.ts
@@ -0,0 +1,32 @@
+export const USERS = [
+  {
+    id: 'user-1',
+    name: 'Rose Kinley-Stapleton',
+    image: 'user-1.jpg',
+  },
+  {
+    id: 'user-2',
+    name: 'Brad Steele',
+    image: 'user-2.jpg',
+  },
+  {
+    id: 'user-3',
+    name: 'Marney Gayle',
+    image: 'user-3.jpg',
+  },
+  {
+    id: 'user-4',
+    name: 'Jeremy Wilcox-Thompson',
+    image: 'user-4.jpg',
+  },
+  {
+    id: 'user-5',
+    name: 'Lynton Moss',
+    image: 'user-5.jpg',
+  },
+  {
+    id: 'user-6',
+    name: 'Lana Edwards',
+    image: 'user-6.jpg',
+  },
+];
