510574f16669a0eba6ed33e4f75f830b4db38003
Nimira43 <n.rabone72@aol.com>
2025-06-05 22:48:23 +0100

    Styed task card.

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core'
+import { Component, Input } from '@angular/core'
+
+
 
 @Component({
   selector: 'app-task',
@@ -8,5 +10,5 @@ import { Component } from '@angular/core'
   styleUrl: './task.component.css'
 })
 export class TaskComponent {
-
+  @Input() task: 
 }
