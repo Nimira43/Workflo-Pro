2d4e7da74131544595a103d764f9e79e25c36fea
Nimira43 <n.rabone72@aol.com>
2025-08-24 02:39:13 +0100

    Importing NewTaskComponent in tasks.component.ts

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core'
 import { TaskComponent } from './task/task.component'
+import { NewTaskComponent } from './new-task/new-task.component'
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
