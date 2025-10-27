c38c53c2f2e3bafe9ef9e1eb6c8e7bd704eed286
Nimira43 <n.rabone72@aol.com>
2025-10-27 03:38:56 +0000

    Corrected mistake in new task html - now closing div before the dialog element, not after it.

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -6,10 +6,11 @@ import { type NewTaskData } from './task/task.model'
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+  styleUrl: './tasks.component.css',
+  imports: [TaskComponent, NewTaskComponent]
 })
+
 export class TasksComponent {
   @Input({required: true}) userId!: string
   @Input({required: true}) name!: string
