cc467b0ad28bc9b20e809eb358752fa1c50d850f
Nimira43 <n.rabone72@aol.com>
2025-12-02 08:44:44 +0000

    Moving the tasks array from TasksComponent to TasksService

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -16,37 +16,7 @@ export class TasksComponent {
   @Input({required: true}) name!: string
   isAddingTask = false
 
-  tasks = [
-    {
-      id: 't1',
-      userId: 'user-1',
-      title: 'Learn Svelte JS',
-      summary: 'Know how to create applications in Svelte using Svelte Kit.',
-      dueDate: '2025-11-17'
-    },
-    {
-      id: 't2',
-      userId: 'user-5',
-      title: 'Learn Mongo DB',
-      summary: 'Know how to integrate Mongo DB into different full stack applications.',
-      dueDate: '2026-03-02'
-    },
-    {
-      id: 't3',
-      userId: 'user-4',
-      title: 'Learn Nest JS',
-      summary: 'Create a full API application using Nest JS.',
-      dueDate: '2026-04-26'
-    },
-    {
-      id: 't4',
-      userId: 'user-2',
-      title: 'Learn Tailwind 4',
-      summary: 'Develop a full understanding of Tailwind 4 for use in Frontend development.',
-      dueDate: '2025-09-22'
-    }
-  ]
-
+  
   get selectedUserTasks() {
     return this.tasks.filter((task) => task.userId === this.userId)
   }
