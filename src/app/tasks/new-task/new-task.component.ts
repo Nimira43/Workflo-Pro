106370ee2e7d66d33c306290b1096b125e0e6e19
Nimira43 <n.rabone72@aol.com>
2025-12-18 04:15:51 +0000

    Input decorator

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, inject, Output } from '@angular/core'
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { type NewTaskData } from '../task/task.model'
 import { TasksService } from '../tasks.service'
@@ -12,6 +12,7 @@ import { TasksService } from '../tasks.service'
 })
 
 export class NewTaskComponent {
+  @Input({required: true}) userId!
   @Output() cancel = new EventEmitter<void>()
   @Output() add = new EventEmitter<NewTaskData>()
   enteredTitle = ''
